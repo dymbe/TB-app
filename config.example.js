7f3154a4cca5b2fe401fbdeb8040dbff6acb9a05
Erik Dymbe <erik.dymbe@gmail.com>
2018-10-09 22:39:33 +0200

    Add config-example

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// Example of how the config.js file might look
+const Config = {
+    doorOpener: {
+        mqttbrokerUrl: 'mqttbroker.com',
+        username: 'hello',
+        password: 'world'
+    }
+};
+
+export default Config
