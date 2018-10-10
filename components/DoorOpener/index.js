67cd02bab9efda7f989a2afe6cfa22c6267df5c6
Erik Dymbe <erik.dymbe@gmail.com>
2018-10-10 13:48:55 +0200

    Create a simple implementation of the door opener
    Will open door when the door-opener screen is opened.

--- components/DoorOpener/index.js
+++ components/DoorOpener/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import { Content } from 'native-base';
-
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {Content} from 'native-base';
+import Config from '../../config';
+import {Client, Message} from 'react-native-paho-mqtt';
 import CustomHeader from '../CustomHeader';
 
 
@@ -10,13 +11,64 @@ export default class DoorOpener extends React.Component {
     drawerLabel: 'Døråpner',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      mqttstuff: 'Heilå',
+      noeAnnet: 'yoyoyo'
+    };
+  }
+
+  componentDidMount() {
+    //Set up an in-memory alternative to global localStorage
+    const myStorage = {
+      setItem: (key, item) => {
+        myStorage[key] = item;
+      },
+      getItem: (key) => myStorage[key],
+      removeItem: (key) => {
+        delete myStorage[key];
+      },
+    };
+
+    // Create a client instance
+    const client = new Client({uri: `wss://${Config.doorOpener.mqttbrokerUrl}:${Config.doorOpener.mqttbrokerPort}/ws`, clientId: 'clientId', storage: myStorage});
+
+    // set event handlers
+    client.on('connectionLost', (responseObject) => {
+      if (responseObject.errorCode !== 0) {
+        console.log(responseObject.errorMessage);
+      }
+    });
+
+    // connect the client
+    const connectOptions = {
+      userName: Config.doorOpener.userName,
+      password: Config.doorOpener.password,
+      useSSL: true
+    };
+
+    client.connect(connectOptions)
+    .then(() => {
+      const message = new Message('tb-appen');
+      message.destinationName = 'teknobyen/doors/front/open';
+      client.send(message);
+    })
+    .catch((responseObject) => {
+      if (responseObject.errorCode !== 0) {
+        console.log('onConnectionLost: ' + responseObject.errorMessage);
+      }
+    })
+    ;
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
-        <CustomHeader title={"Døråpner"} icon={"ios-arrow-back"} navigate={() => this.props.navigation.goBack(null)} />
+        <CustomHeader title={'Døråpner'} icon={'ios-arrow-back'} navigate={() => this.props.navigation.goBack(null)}/>
 
         <View style={styles.content}>
-          <Text>Døråpnersiden</Text>
+          <Text>Nå bør døren åpne seg...</Text>
         </View>
       </View>
     );
--- config.example.js
+++ config.example.js
@@ -1,10 +1,11 @@
 // Example of how the config.js file might look
 const Config = {
-    doorOpener: {
-        mqttbrokerUrl: 'mqttbroker.com',
-        username: 'hello',
-        password: 'world'
-    }
+  doorOpener: {
+    mqttbrokerUrl: 'mqttbroker.com',
+    mqttbrokerPort: 80,
+    userName: 'hello',
+    password: 'world'
+  }
 };
 
 export default Config
